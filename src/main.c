7a6f6140e6bbbd4edb3e61f60d6429526652cb5d
Florent Jardin <florent.jardin@gmail.com>
2026-07-17 17:54:34 +0200

    feat: integrate UI modules into combat loop

--- src/main.c
+++ src/main.c
@@ -2,7 +2,27 @@
 #include "ui/log.h"
 #include "ui/bar.h"
 #include "ui/menu.h"
+#include "entity.h"
+#include "combat.h"
+#include "monster.h"
 #include <curses.h>
+
+static void refresh_screen(WINDOW *game, struct layout const *ly,
+                           struct entity const *player,
+                           struct entity const *monster,
+                           struct log const *log)
+{
+    struct entity_view pv = { .name = player->name, .hp = player->health, .max = player->health_max };
+    struct entity_view mv = { .name = monster->name, .hp = monster->health, .max = monster->health_max };
+
+    werase(game);
+    box(game, 0, 0);
+    entity_view_draw(game, BOX_W, ly->hp_y, &pv, &mv);
+    draw_separator(game, ly->sep1_y, BOX_W);
+    log_draw(log, game, ly->log_y, BOX_W, LOG_LINES);
+    draw_separator(game, ly->sep2_y, BOX_W);
+    wnoutrefresh(game);
+}
 
 int main(void)
 {
@@ -12,43 +32,66 @@ int main(void)
     getmaxyx(stdscr, term_rows, term_cols);
 
     struct layout ly = layout_make(term_rows, term_cols);
-
     WINDOW *game = ui_window(&ly);
+
+    struct entity player;
+    entity_init(&player, "Player", 30, 5, 10);
+
+    struct entity monster;
+    monster_spawn(&monster, MONSTER_GOBLIN);
 
     struct log log;
     log_init(&log);
     log_add(&log, "Bienvenue dans le donjon !");
+    log_add(&log, "Un %s sauvage apparait !", monster.name);
 
-    struct entity_view player = { .name = "Player", .hp = 20, .max = 30 };
-    struct entity_view goblin  = { .name = "Gobelin", .hp = 5, .max = 10 };
-
-    werase(game);
-    box(game, 0, 0);
-    entity_view_draw(game, BOX_W, ly.hp_y, &player, &goblin);
-    draw_separator(game, ly.sep1_y, BOX_W);
-    log_draw(&log, game, ly.log_y, BOX_W, LOG_LINES);
-    draw_separator(game, ly.sep2_y, BOX_W);
     struct menu menu;
     menu_init(&menu);
     menu_add(&menu, "Attaquer");
     menu_add(&menu, "Inventaire");
 
-    wnoutrefresh(game);
+    refresh_screen(game, &ly, &player, &monster, &log);
     doupdate();
 
-    int sel;
-    do
+    while (player.health > 0 && monster.health > 0)
     {
-        sel = menu_run(&menu, game, 2, ly.menu_y);
-        if (sel >= 0)
-        {
-            log_add(&log, "%s", menu.items[sel].label);
-            log_draw(&log, game, ly.log_y, BOX_W, LOG_LINES);
-            wnoutrefresh(game);
-            doupdate();
-        }
-    } while (sel >= 0);
+        int sel = menu_run(&menu, game, 2, ly.menu_y);
 
+        if (sel == 0)
+        {
+            entity_attack(&player, &monster);
+            log_add(&log, "Vous attaquez le %s !", monster.name);
+
+            if (monster.health > 0)
+            {
+                entity_attack(&monster, &player);
+                log_add(&log, "Le %s vous attaque !", monster.name);
+            }
+        }
+        else if (sel == 1)
+        {
+            log_add(&log, "Rien a inventorier pour l'instant.");
+        }
+        else
+        {
+            break;
+        }
+
+        refresh_screen(game, &ly, &player, &monster, &log);
+        doupdate();
+    }
+
+    if (monster.health == 0)
+        log_add(&log, "Le %s est vaincu !", monster.name);
+    else if (player.health == 0)
+        log_add(&log, "Vous etes mort...");
+
+    refresh_screen(game, &ly, &player, &monster, &log);
+    doupdate();
+    wgetch(game);
+
+    entity_destroy(&player);
+    entity_destroy(&monster);
     ui_teardown(game);
     return 0;
 }
